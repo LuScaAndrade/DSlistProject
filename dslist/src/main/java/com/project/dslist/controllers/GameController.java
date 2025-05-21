770fcfcd7628a40efefae11de5c85832e7bfcb1f
LuScaAndrade <Lucasavitorino1@gmail.com>
2025-05-21 15:48:47 -0300

    feature: creating GameController

---
+++ dslist/src/main/java/com/project/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.project.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.dslist.dto.GameMinDTO;
+import com.project.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAllGames();
+		return result;
+	}
+}
--- dslist/src/main/java/com/project/dslist/services/GameService.java
+++ dslist/src/main/java/com/project/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.project.dslist.dto.GameMinDTO;
 import com.project.dslist.entities.Game;
 import com.project.dslist.repositories.GameRepository;
 
@@ -14,8 +15,10 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<Game> findAll() {
+	public List<GameMinDTO> findAllGames() {
 		List<Game> result = gameRepository.findAll();
-		return result;
+		
+		//Transforma uma lista que era de Games para uma lista de DTO
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
