f0f21cc294dcfc681924fff8709b12edc4eabcc8
LuScaAndrade <Lucasavitorino1@gmail.com>
2025-05-26 11:06:31 -0300

    feature: creating controller, DTO and service classes for GameList

---
+++ dslist/src/main/java/com/project/dslist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.project.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.dslist.dto.GameListDTO;
+import com.project.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public List<GameListDTO> findAll() {
+		List<GameListDTO> result = gameListService.findAllGames();
+		return result;
+	}
+}
