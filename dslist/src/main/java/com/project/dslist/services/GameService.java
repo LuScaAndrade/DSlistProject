f0083c4708b4b018c8976cb139abeef4d4ace0d0
LuScaAndrade <Lucasavitorino1@gmail.com>
2025-05-26 10:40:08 -0300

    feature: adding new search by id method

--- dslist/src/main/java/com/project/dslist/services/GameService.java
+++ dslist/src/main/java/com/project/dslist/services/GameService.java
@@ -3,8 +3,11 @@ package com.project.dslist.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.project.dslist.dto.GameDTO;
 import com.project.dslist.dto.GameMinDTO;
 import com.project.dslist.entities.Game;
 import com.project.dslist.repositories.GameRepository;
@@ -15,6 +18,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	private GameDTO findById(Long id) {
+		try {
+			Game result = gameRepository.findById(id).get();
+			return new GameDTO(result);
+		} catch(DataAccessResourceFailureException e) {
+			throw new RuntimeException("Não foi possível encontrar o id especificado: ", e);
+		}
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAllGames() {
 		List<Game> result = gameRepository.findAll();
 		
